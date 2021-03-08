e409a9fe4f013c0d7c87ce70bd076a4e8dd14bdf
AlexPlex1 <61334247+AlexPlex1@users.noreply.github.com>
2021-03-08 19:57:39 +0200

    Create changeLocation.php

---
+++ php_course/Save the farm!/changeLocation.php
@@ -0,0 +1,25 @@
+<?php
+  // Change player location
+function changeLocation(){	
+  // Write your code here:
+  global $location;
+  echo "Where do you want to go?\n";
+  $where_to_go = strtolower(readline(">> "));
+  if ($location === "kitchen" && $where_to_go === "bathroom"){
+    echo "You go to: bathroom.\n";
+    $location = "bathroom";
+  } elseif ($location === "kitchen" && $where_to_go === "woods"){
+    echo "You follow the winding path, shivering as you make your way deep into the Terror Woods.\n";
+    $location = "woods";
+  } elseif ($location === "bathroom" && $where_to_go === "kitchen"){
+    echo "You go to: kitchen.\n";
+    $location = "kitchen";
+  } elseif ($location === "woods" && $where_to_go === "kitchen"){
+    echo "You go to: kitchen.\n";
+    $location = "kitchen";
+  } elseif ($where_to_go === "woods" || $where_to_go === "kitchen" ||$where_to_go === "bathroom"){
+    echo "You can't go directly to there from your current location. Try going somewhere else first.\n";
+  } else {
+    echo "That doesn't make sense. Are you confused? Try 'look around'.\n";
+  }
+}
